18c3f5e3b551ba2f3d17664c04b41e3d6ccc6a79
ViniciusLibano <vinicius2010libano@gmail.com>
2022-05-07 15:24:58 -0300

    Calculo da idade gestacional

--- script.js
+++ script.js
@@ -16,19 +16,20 @@ function showCiclo() {
 function calc() {
     let resultado = document.querySelectorAll('p');
     let divContent = document.querySelector('main');
-
     let dumValue = document.querySelector('input[name="DUM"]').value;
 
     let dumDate = new Date(dumValue);
     let today = new Date();
     let timeDiff = Math.abs(dumDate.getTime() - today.getTime());
     let diffDays = Math.ceil(timeDiff / (1000*3600*24));
+    let idadeGestacionalSemana = Math.floor(diffDays / 7);
+    let idadeGestacionalDia = diffDays % 7;
 
     if (dumValue.length == 0) {
-        window.alert("ta em branco tio")
+        window.alert("Data em branco");
     }
     else if ((!resultado[0]) || (resultado[0].innerText != dumValue)) {
-        divContent.insertAdjacentHTML('beforeend', `<p>${dumValue}</p><p>${diffDays} Dias de diferença</p>`);
+        divContent.insertAdjacentHTML('beforeend', `<p>${dumValue}</p><p>Idade Gestacional: ${idadeGestacionalSemana} Semana(s) e ${idadeGestacionalDia} Dia(s)</p>`);
         console.log(`${dumDate.getTime()} - ${today.getTime()} = ${timeDiff}`);
         resultado[0].remove();
         resultado[1].remove();
